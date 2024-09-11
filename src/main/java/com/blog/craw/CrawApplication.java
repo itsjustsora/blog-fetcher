9fd2ef3a7544d9c826d8ea0835f5c3cecb3eb55f
keemsora <itsyoursora@gmail.com>
2024-09-11 22:47:42 +0900

    initial commit

---
+++ src/main/java/com/blog/craw/CrawApplication.java
@@ -0,0 +1,13 @@
+package com.blog.craw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawApplication.class, args);
+	}
+
+}
