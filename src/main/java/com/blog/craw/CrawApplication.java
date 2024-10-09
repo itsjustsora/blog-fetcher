d8c12c514a8accbc6995a71c07571ca8cae2689e
keemsora <itsyoursora@gmail.com>
2024-10-09 20:12:11 +0900

    chore: 작업 완료 후 애플리케이션 종료 설정

--- src/main/java/com/blog/craw/CrawApplication.java
+++ src/main/java/com/blog/craw/CrawApplication.java
@@ -6,6 +6,9 @@ import org.springframework.context.ApplicationContext;
 
 import com.blog.craw.service.WebContentService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class CrawApplication {
 
@@ -15,6 +18,14 @@ public class CrawApplication {
 
 		// WebContentService 인스턴스를 가져와서 updateReadme() 메소드 호출
 		WebContentService webContentService = context.getBean(WebContentService.class);
-		webContentService.updateReadme();
+
+		try {
+			webContentService.updateReadme();
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		} finally {
+			// 작업 완료 후 Spring의 Bean과 자원 정리 및 애플리케이션 종료
+			SpringApplication.exit(context, () -> 0);
+		}
 	}
 }
